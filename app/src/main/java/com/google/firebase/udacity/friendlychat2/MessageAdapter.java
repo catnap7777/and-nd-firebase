866303cc8d101686929c076523ee79cb648a28ca
kam <student>
2019-09-14 00:22:38 -0500

    update to my fork

--- app/src/main/java/com/google/firebase/udacity/friendlychat2/MessageAdapter.java
+++ app/src/main/java/com/google/firebase/udacity/friendlychat2/MessageAdapter.java
@@ -1,4 +1,4 @@
-package com.google.firebase.udacity.friendlychat;
+package com.google.firebase.udacity.friendlychat2;
 
 import android.app.Activity;
 import android.content.Context;
@@ -9,6 +9,7 @@ import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.bumptech.glide.Glide;
+
 
 import java.util.List;
 
